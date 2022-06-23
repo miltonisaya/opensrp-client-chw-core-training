31f911809413f09ec01e8a8dc89304ef9e15e239
Billy <billypatrick767@gmail.com>
2022-06-23 16:13:11 +0300

    added: updateDetails util for shared update details methods

---
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/utils/UpdateDetailsUtil.java
@@ -0,0 +1,44 @@
+package org.smartregister.chw.core.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import com.vijay.jsonwizard.constants.JsonFormConstants;
+import com.vijay.jsonwizard.domain.Form;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONObject;
+import org.smartregister.commonregistry.CommonPersonObject;
+import org.smartregister.commonregistry.CommonPersonObjectClient;
+import org.smartregister.opd.activity.BaseOpdFormActivity;
+import org.smartregister.opd.utils.OpdConstants;
+
+import static org.smartregister.chw.core.utils.CoreReferralUtils.getCommonRepository;
+
+public class UpdateDetailsUtil {
+    @NotNull
+    public static CommonPersonObjectClient getFamilyRegistrationDetails(String familyBaseEntityId) {
+        //Update common person client object with all details from family register table
+        final CommonPersonObject personObject = getCommonRepository(Utils.metadata().familyRegister.tableName)
+                .findByBaseEntityId(familyBaseEntityId);
+        CommonPersonObjectClient commonPersonObjectClient = new CommonPersonObjectClient(personObject.getCaseId(),
+                personObject.getDetails(), "");
+        commonPersonObjectClient.setColumnmaps(personObject.getColumnmaps());
+        commonPersonObjectClient.setDetails(personObject.getDetails());
+        return commonPersonObjectClient;
+    }
+
+    public static void startUpdateClientDetailsActivity(JSONObject jsonForm, Activity activity) {
+        Intent intent = new Intent(activity, BaseOpdFormActivity.class);
+        intent.putExtra(OpdConstants.JSON_FORM_EXTRA.JSON, jsonForm.toString());
+        Form form = new Form();
+        form.setName(activity.getString(org.smartregister.chw.core.R.string.update_client_registration));
+        form.setActionBarBackground(org.smartregister.chw.core.R.color.family_actionbar);
+        form.setNavigationBackground(org.smartregister.chw.core.R.color.family_navigation);
+        form.setHomeAsUpIndicator(org.smartregister.chw.core.R.mipmap.ic_cross_white);
+        form.setPreviousLabel(activity.getResources().getString(org.smartregister.chw.core.R.string.back));
+        form.setWizard(false);
+        intent.putExtra(JsonFormConstants.JSON_FORM_KEY.FORM, form);
+        activity.startActivityForResult(intent, org.smartregister.family.util.JsonFormUtils.REQUEST_CODE_GET_JSON);
+    }
+}
