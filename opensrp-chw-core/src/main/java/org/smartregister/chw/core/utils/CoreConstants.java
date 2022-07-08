e352d573f7f59426b5f7bf8b746543fc8b0ec12c
Billy <billypatrick767@gmail.com>
2022-07-08 14:58:12 +0300

    defined: focus for LTFU feedback

--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/utils/CoreConstants.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/utils/CoreConstants.java
@@ -1198,6 +1198,7 @@ public class CoreConstants {
         public static final String SUSPECTED_CHILD_GBV = "Suspected Child GBV";
         public static final String ANC_MALE_ENGAGEMENT = "ANC Male engagement";
         public static final String LTFU = "LTFU";
+        public static final String LTFU_FEEDBACK= "LTFU Community Followup Feedback";
     }
 
     public static final class REGISTER_TYPE {
