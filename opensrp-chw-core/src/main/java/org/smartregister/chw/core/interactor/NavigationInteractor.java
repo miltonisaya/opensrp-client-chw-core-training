0fecaf58b9e114a8dec2129645a35bfdeaf5058d
Ilakoze Jumanne <ilakozejumanne@gmail.com>
2022-07-12 12:09:37 +0300

    :construction: refactored referrals query count

--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/interactor/NavigationInteractor.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/interactor/NavigationInteractor.java
@@ -5,9 +5,11 @@ import org.smartregister.chw.core.contract.NavigationContract;
 import org.smartregister.chw.core.custom_views.NavigationMenu;
 import org.smartregister.chw.core.dao.NavigationDao;
 import org.smartregister.chw.core.utils.CoreConstants;
+import org.smartregister.chw.core.utils.Utils;
 import org.smartregister.chw.fp.util.FamilyPlanningConstants;
 import org.smartregister.chw.referral.util.Constants;
 import org.smartregister.family.util.AppExecutors;
+import org.smartregister.repository.AllSharedPreferences;
 
 import java.util.Date;
 
@@ -326,12 +328,16 @@ public class NavigationInteractor implements NavigationContract.Interactor {
                 return NavigationDao.getQueryCount(allClients);
 
             case Constants.Tables.REFERRAL:
+                AllSharedPreferences allSharedPreferences = Utils.getAllSharedPreferences();
+                String anm = allSharedPreferences.fetchRegisteredANM();
+                String currentLoaction = allSharedPreferences.fetchUserLocalityId(anm);
                 String sqlReferral = "select count(*) " +
                         "from " + Constants.Tables.REFERRAL + " p " +
                         "inner join ec_family_member m on p.entity_id = m.base_entity_id COLLATE NOCASE " +
                         "inner join ec_family f on f.base_entity_id = m.relational_id COLLATE NOCASE " +
                         "inner join task t on p.id = t.reason_reference COLLATE NOCASE " +
                         "where m.date_removed is null and t.business_status = '" + CoreConstants.BUSINESS_STATUS.REFERRED + "' " +
+                        "AND t.location <> '" + currentLoaction + "' COLLATE NOCASE " +
                         "AND p.chw_referral_service <> 'LTFU' COLLATE NOCASE ";
                 return NavigationDao.getQueryCount(sqlReferral);
 
