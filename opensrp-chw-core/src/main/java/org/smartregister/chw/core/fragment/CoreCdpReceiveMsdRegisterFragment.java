c5deb9d3a67a4d3a3638553b3060428818491d99
Billy <billypatrick767@gmail.com>
2022-09-10 11:53:38 +0300

    added: navigation menu instance for the ReceiveMSD Fragment

---
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/fragment/CoreCdpReceiveMsdRegisterFragment.java
@@ -0,0 +1,30 @@
+package org.smartregister.chw.core.fragment;
+
+import android.view.View;
+
+import org.smartregister.chw.cdp.fragment.BaseCdpReceiveMsdRegisterFragment;
+import org.smartregister.chw.core.custom_views.NavigationMenu;
+
+import timber.log.Timber;
+
+public class CoreCdpReceiveMsdRegisterFragment extends BaseCdpReceiveMsdRegisterFragment {
+    @Override
+    public void setupViews(View view) {
+        super.setupViews(view);
+        try {
+            NavigationMenu.getInstance(getActivity(), null, toolbar);
+        } catch (NullPointerException e) {
+            Timber.e(e);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        try {
+            NavigationMenu.getInstance(getActivity(), null, toolbar);
+        } catch (NullPointerException e) {
+            Timber.e(e);
+        }
+    }
+}
