6aedf34af1491693d137837a3c9b493dd0e2a4d2
Ilakoze Jumanne <ilakozejumanne@gmail.com>
2022-12-01 11:26:37 +0300

    :hammer: refactored save visits in CoreAncHomeVisitInteractor

--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/interactor/CoreAncHomeVisitInteractor.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/interactor/CoreAncHomeVisitInteractor.java
@@ -1,11 +1,21 @@
 package org.smartregister.chw.core.interactor;
 
+import com.google.gson.Gson;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.Nullable;
+import org.smartregister.chw.anc.AncLibrary;
 import org.smartregister.chw.anc.contract.BaseAncHomeVisitContract;
 import org.smartregister.chw.anc.domain.MemberObject;
+import org.smartregister.chw.anc.domain.Visit;
 import org.smartregister.chw.anc.interactor.BaseAncHomeVisitInteractor;
 import org.smartregister.chw.anc.model.BaseAncHomeVisitAction;
+import org.smartregister.chw.anc.util.JsonFormUtils;
+import org.smartregister.chw.anc.util.NCUtils;
 import org.smartregister.chw.anc.util.VisitUtils;
 import org.smartregister.chw.core.dao.AncDao;
+import org.smartregister.clientandeventmodel.Event;
+import org.smartregister.repository.AllSharedPreferences;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -60,5 +70,51 @@ public class CoreAncHomeVisitInteractor extends BaseAncHomeVisitInteractor {
 
         LinkedHashMap<String, BaseAncHomeVisitAction> calculateActions(final BaseAncHomeVisitContract.View view, MemberObject memberObject, final BaseAncHomeVisitContract.InteractorCallBack callBack) throws BaseAncHomeVisitAction.ValidationException;
 
+    }
+
+    @Override
+    protected @Nullable Visit saveVisit(boolean editMode, String memberID, String encounterType,
+                                        final Map<String, String> jsonString,
+                                        String parentEventType
+    ) throws Exception {
+
+        AllSharedPreferences allSharedPreferences = AncLibrary.getInstance().context().allSharedPreferences();
+
+        String derivedEncounterType = StringUtils.isBlank(parentEventType) ? encounterType : "";
+        Event baseEvent = JsonFormUtils.processVisitJsonForm(allSharedPreferences, memberID, derivedEncounterType, jsonString, getTableName());
+
+        // only tag the first event with the date
+        if (StringUtils.isBlank(parentEventType)) {
+            prepareEvent(baseEvent);
+        } else {
+            prepareSubEvent(baseEvent);
+        }
+
+        if (baseEvent != null) {
+            baseEvent.setFormSubmissionId(JsonFormUtils.generateRandomUUIDString());
+            JsonFormUtils.tagEvent(allSharedPreferences, baseEvent);
+
+            String visitID = (editMode) ?
+                    visitRepository().getLatestVisit(memberID, getEncounterType()).getVisitId() :
+                    JsonFormUtils.generateRandomUUIDString();
+
+            // reset database
+            if (editMode) {
+                Visit visit = visitRepository().getVisitByVisitId(visitID);
+                if (visit != null)
+                    baseEvent.setEventDate(visit.getDate());
+
+                deleteProcessedVisit(visitID, memberID);
+                deleteOldVisit(visitID);
+            }
+
+            Visit visit = NCUtils.eventToVisit(baseEvent, visitID);
+            visit.setPreProcessedJson(new Gson().toJson(baseEvent));
+            visit.setParentVisitID(getParentVisitEventID(visit, parentEventType));
+
+            visitRepository().addVisit(visit);
+            return visit;
+        }
+        return null;
     }
 }
