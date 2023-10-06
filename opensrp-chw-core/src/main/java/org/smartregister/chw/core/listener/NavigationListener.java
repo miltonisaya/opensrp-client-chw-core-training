1da9b29311684f6a4e6ec85f63eae009576145fc
ChrissDisigale <chrispro.genius@gmail.com>
2023-10-06 13:54:58 +0300

    :hammer: edited listerner

--- opensrp-chw-core/src/main/java/org/smartregister/chw/core/listener/NavigationListener.java
+++ opensrp-chw-core/src/main/java/org/smartregister/chw/core/listener/NavigationListener.java
@@ -122,6 +122,9 @@ public class NavigationListener implements View.OnClickListener {
                 case CoreConstants.DrawerMenu.REPORTS:
                     startRegisterActivity(getActivity(CoreConstants.REGISTERED_ACTIVITIES.REPORTS));
                     break;
+                case CoreConstants.DrawerMenu.VMMC:
+                    startRegisterActivity(getActivity(CoreConstants.REGISTERED_ACTIVITIES.VMMC_REGISTER_ACTIVITY));
+                    break;
                 default:
                     Utils.showShortToast(activity.getApplicationContext(), "Unspecified navigation action");
                     break;
